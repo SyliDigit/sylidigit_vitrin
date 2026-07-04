7e0653ee536743ca2d50cedd9232e0ccf38e14c1
SyliDigit <sylidigit@gmail.com>
2026-07-04 14:47:06 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, // Recommandé pour l'export statique Next.js
     domains: ['images.pexels.com'],
   },
   devIndicators: false,
+  output: 'export', // <-- Cette ligne indique à Next.js de générer du HTML pur
 }
 
 module.exports = nextConfig
